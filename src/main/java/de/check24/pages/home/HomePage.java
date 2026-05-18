6429b5186ac0c74e11cdcf944251ae7a6b5dc65e
dzmitrykulkou <dzmitry.kulkou@innowise.com>
2026-05-18 14:18:27 +0300

    add test with allure annotation

--- src/main/java/de/check24/pages/home/HomePage.java
+++ src/main/java/de/check24/pages/home/HomePage.java
@@ -1,16 +1,22 @@
 package de.check24.pages.home;
 
 import de.check24.pages.BasePage;
+import de.check24.pages.common.FooterComponent;
 import de.check24.pages.common.HeaderComponent;
 import lombok.Getter;
 
 public class HomePage extends BasePage {
     @Getter
     private final HeaderComponent headerComponent = new HeaderComponent();
+    @Getter
+    private final FooterComponent footerComponent = new FooterComponent();
 
     public void search(String searchText) {
         headerComponent.fillSearchInput(searchText);
         headerComponent.clickSearchButton();
     }
 
+    public String getCopyrightText() {
+        return footerComponent.getCopyrightText();
+    }
 }
