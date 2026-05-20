c3426093deab9cb7f908853b6d47da9c1e0d0246
dzmitrykulkou <dzmitry.kulkou@innowise.com>
2026-05-20 15:45:38 +0300

    add second request

--- src/main/java/de/check24/api/clients/UserClient.java
+++ src/main/java/de/check24/api/clients/UserClient.java
@@ -6,14 +6,50 @@ import static io.restassured.RestAssured.given;
 
 public class UserClient {
     private static final String EXISTS_EXTENDED_PATH = "/login/api/user/existsextended/";
+    private static final String CSRF_TOKEN_PATH = "login/api/csrftoken/";
+    private static final String AUTH_PROCESS_START_PATH =  "login/api/user/authprocessstart/";
+    private static final String AUTH_PROCESS_CONTINUE_PATH =  "login/api/user/authprocesscontinue/";
+    private static final String CSRF_TOKEN = getCsrfToken();
 
-    public Response checkUserExistsExtended(String csrfToken, String product, String email) {
+    public Response checkUserExistsExtended(String product, String email) {
         return given()
                 .contentType("application/x-www-form-urlencoded; charset=UTF-8")
-                .cookie("csrf_token", csrfToken)
+                .cookie("csrf_token", CSRF_TOKEN)
                 .formParam("product", product)
-                .formParam("csrf_token", csrfToken)
+                .formParam("csrf_token", CSRF_TOKEN)
                 .formParam("a", email)
                 .post(EXISTS_EXTENDED_PATH);
+    }
+
+    private static String getCsrfToken() {
+        return given()
+                .post(CSRF_TOKEN_PATH)
+                .getBody()
+                .jsonPath()
+                .getString("csrfToken");
+    }
+
+    public Response firstPasswordRequest(String authType, String email) {
+        return given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .cookie("csrf_token", CSRF_TOKEN)
+                .formParam("auth_type", authType)
+                .formParam("csrf_token", CSRF_TOKEN)
+                .formParam("login", email)
+                .post(AUTH_PROCESS_START_PATH);
+    }
+
+    public String getUUID(Response response) {
+        return response.jsonPath().getString("data.authprocessinstance_uuid");
+    }
+
+    public Response secondPasswordRequest(String uuid) {
+        return given()
+                .contentType("application/x-www-form-urlencoded; charset=UTF-8")
+                .cookie("csrf_token", CSRF_TOKEN)
+                .formParam("uuid", uuid)
+                .formParam("csrf_token", CSRF_TOKEN)
+                .formParam("token", "")
+                .post(AUTH_PROCESS_CONTINUE_PATH);
     }
 }
--- src/test/java/de/check24/tests/api/UserApiTest.java
+++ src/test/java/de/check24/tests/api/UserApiTest.java
@@ -10,17 +10,32 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserApiTest extends BaseApiTest {
     private final UserClient userClient = new UserClient();
+    private static final String email = "m1loz0l1wuj3@mailtowin.com";
 
     @Test
     @DisplayName("Check user exists API test")
     void checkUserExistsExtendedTest() {
-        String csrfToken = "f7a8f73ba236b8e7bac49e77ad74fb1fc754d55c737611f4ef22034fafc07ff8.yfGVAoZ6Hhjv5b23P-CLmUN-Ik2QkTDSv9EU8Ffkg7g";
         String product = "hotel";
-        String email = "m1loz0l1wuj3@mailtowin.com";
-
-        Response response = userClient.checkUserExistsExtended(csrfToken, product, email);
+        Response response = userClient.checkUserExistsExtended(product, email);
 
         assertThat(response.statusCode())
                 .isEqualTo(200);
+    }
+
+    @Test
+    @DisplayName("Auth processes")
+    void testAuthProcesses() {
+        String authType = "password_v3";
+        Response response1 = userClient
+                .firstPasswordRequest(authType, email);
+
+        assertThat(response1.statusCode()).isEqualTo(200);
+
+        String uuid = userClient.getUUID(response1);
+
+        Response response2 = userClient
+                .secondPasswordRequest(uuid);
+
+        assertThat(response2.statusCode()).isEqualTo(200);
     }
 }
